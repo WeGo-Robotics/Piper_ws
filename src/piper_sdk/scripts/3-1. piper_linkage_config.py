98994aa4b7f36b0f8ee759d2b503c6c8f77f3261
Geonwoo <gw.cho@wego-robotics.com>
2025-03-24 17:27:01 +0900

    Update 3-1. piper_linkage_config.py

--- src/piper_sdk/scripts/3-1. piper_linkage_config.py
+++ src/piper_sdk/scripts/3-1. piper_linkage_config.py
@@ -2,15 +2,15 @@
 # -*-coding:utf8-*-
 
 """
-해당 파일을 실행하기 전에 로봇팔을 안전한 위치로 이동시키고, Disable() 함수를 사용하여 PiPER를 비활성화해야 합니다.
-Python SDK에서도 모드 전환이 가능하지만, 사용을 권장하지 않습니다.
+해당 파일을 실행하기 전에 로봇팔을 안전한 위치로 이동시키고, PiPER를 비활성화해야 합니다.
+0-2. piper_emergency_stop.py 파일을 클릭 후 Master-Slave 모드 변환을 해주세요.
 """
 
 from typing import Optional
 from piper_sdk import *
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+
 if __name__ == "__main__":
-    piper = C_PiperInterface()
-    piper.ConnectPort()  
-    # piper.MasterSlaveConfig(0xFA, 0, 0, 0) # Mater
-    piper.MasterSlaveConfig(0xFC, 0, 0, 0) # Slave
+    piper = C_PiperInterface_V2()
+    piper.ConnectPort()
+    # piper.MasterSlaveConfig(0xFA, 0, 0, 0)  # Mater
+    piper.MasterSlaveConfig(0xFC, 0, 0, 0)  # Slave
